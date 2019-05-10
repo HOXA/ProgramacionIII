88ed99ebe64e01ff8a02da70377631432dff6bb8
HOXA <rsandovale@miumg.edu.gt>
2019-05-10 00:15:48 -0600

    Update 2

--- TorresDeHanoi/src/torresdehanoi/Hanoi.java
+++ TorresDeHanoi/src/torresdehanoi/Hanoi.java
@@ -3,12 +3,19 @@ package torresdehanoi;
 import java.util.*;
 public class Hanoi {
     public static void main(String[] args) {
+        Scanner resp = new Scanner(System.in);
+        int rp=1;
+        while(rp==1){
         Scanner sc = new Scanner(System.in); //Leer numero de discos a utilizar
         int n;
         System.out.println("Numero de discos: ");
         n = sc.nextInt();
         Hanoi(n,1,2,3);  //1:origen  2:auxiliar 3:destino
+        System.out.print("Desea jugar de nuevo?(1 S1, 0 No): ");
+        rp=resp.nextInt();
+        }
     }
+        
 
 
 public static void Hanoi(int n, int origen,  int auxiliar, int destino){
